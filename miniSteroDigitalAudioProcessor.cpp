3cc897890620b2d2cffb42eac418d98ec6a8ea94
GaneshChintalapaty <ganesh.chintalapaty@gmail.com>
2024-02-08 19:07:28 -0600

    MSDAP_08022024_1907

--- miniSteroDigitalAudioProcessor.cpp
+++ miniSteroDigitalAudioProcessor.cpp
@@ -17,9 +17,9 @@
 #pragma region Global Variables
 
 //Include Vector variables here
-std::vector<int> hCoeff;
-std::vector<int> xData;
-std::vector<int> yOutput;
+std::vector<uint32_t> hCoeff;
+std::vector<uint16_t> xData;
+std::vector<uint64_t> yOutput;
 //End Vector variables
 
 #pragma endregion
@@ -29,6 +29,60 @@ std::vector<int> yOutput;
 int parse(std::string filePath, uint8_t selectVectorToStoreData)
 {
     std::ifstream inputFile(filePath);
+
+    if(inputFile.is_open())
+    {
+        std::string line;
+        while(std::getline(inputFile, line))
+        {
+            std::istringstream iss(line);
+            std::string hexString;
+            iss >> hexString;
+
+            switch (selectVectorToStoreData)
+            {
+            case 1:
+                {
+                    uint32_t hexCoeff;
+                    std::stringstream(hexString) >> std::hex >> hexCoeff;
+                    hCoeff.push_back(hexCoeff);
+                }
+                break;
+            case 2:
+                {
+                    uint16_t hexData;
+                    std::stringstream(hexString) >> std::hex >>hexData;
+                    xData.push_back(hexData);
+                }
+                break;
+            
+            default:
+                break;
+            }
+        }
+        inputFile.close();
+
+        std::cout << "Hex numbers stored in hCoeff:" << std::endl;
+
+        for(int num : hCoeff)
+        {
+            std::cout << std::hex << num << std::endl;
+        }
+
+        std::cout << "Hex numbers stored in xData:" << std::endl;
+
+        for(int num : xData)
+        {
+            std::cout << std::hex << num << std::endl;
+        }
+
+    }
+    else
+    {
+        std::cerr << "Failed to open the file." << std::endl;
+        return 1;
+    }
+    return 0;
 }
 
 #pragma endregion
@@ -46,6 +100,20 @@ int main(int argc, char *argv[])
     int fileParseStatus;
 
     fileParseStatus = parse(argv[1], 1);
+
+    if(fileParseStatus == 1)
+    {
+        return 1;
+    }
+
+    fileParseStatus = parse(argv[2], 2);
+
+    if(fileParseStatus == 1)
+    {
+        return 1;
+    }
+
+    return 0;
 
 }
 
